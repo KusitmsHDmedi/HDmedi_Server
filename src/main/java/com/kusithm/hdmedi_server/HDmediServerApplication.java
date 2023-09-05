aeba297a5311c7eaeed9b1cf73fbd4cd406edb6a
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-05 13:13:38 +0900

    [chore] #1 gitignore 추가

---
+++ src/main/java/com/kusithm/hdmedi_server/HDmediServerApplication.java
@@ -0,0 +1,13 @@
+package com.kusithm.hdmedi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HDmediServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HDmediServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kusithm/hdmedi_server/HDmediServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusithm.hdmedi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HDmediServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
