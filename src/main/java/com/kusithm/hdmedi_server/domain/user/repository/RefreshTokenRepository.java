6c2763e08f90311462138b12471b741afef69ce3
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-08 02:44:59 +0900

    Revert "[feat] #5 redis repository & refresh token 구현"
    This reverts commit 2e77f98ac3bfccd86e883cd7a58b578bba62e9ae.

--- src/main/java/com/kusithm/hdmedi_server/domain/user/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kusithm.hdmedi_server.domain.user.repository;
-
-import com.kusithm.hdmedi_server.domain.user.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
-}
