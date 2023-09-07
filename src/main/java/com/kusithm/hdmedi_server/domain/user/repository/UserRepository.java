178dc725d4cfd8c919a76bf3d3315024f0b38109
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-08 02:16:46 +0900

    [feat] #8 jpa userRepository 구현

---
+++ src/main/java/com/kusithm/hdmedi_server/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.kusithm.hdmedi_server.domain.user.repository;
+
+import com.kusithm.hdmedi_server.domain.user.domain.Platform;
+import com.kusithm.hdmedi_server.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByPlatformAndPlatformId(Platform platform, String platformId);
+}
