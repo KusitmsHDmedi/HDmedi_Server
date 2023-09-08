34abae03dc19323a6c975c832922415b31ae1de8
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-08 21:35:17 +0900

    [feat] #11 google smtp 전송 실패 예외 추가

--- src/main/java/com/kusithm/hdmedi_server/global/error/exception/ErrorCode.java
+++ src/main/java/com/kusithm/hdmedi_server/global/error/exception/ErrorCode.java
@@ -12,6 +12,11 @@ public enum ErrorCode {
      * 400 Bad Request
      */
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    /**
+     * 401 Unauthorized
+     */
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요."),
     INVALID_ACCESS_TOKEN_VALUE(HttpStatus.UNAUTHORIZED, "액세스 토큰의 값이 올바르지 않습니다."),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다. 재발급 받아주세요."),
@@ -19,11 +24,7 @@ public enum ErrorCode {
     INVALID_REFRESH_TOKEN_VALUE(HttpStatus.UNAUTHORIZED, "리프레시 토큰의 값이 올바르지 않습니다."),
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 만료되었습니다. 다시 로그인해 주세요."),
     NOT_MATCH_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "일치하지 않는 리프레시 토큰입니다."),
-
-    /**
-     * 401 Unauthorized
-     */
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
+    UNAUTHORIZED_GOOGLE_SMTP(HttpStatus.UNAUTHORIZED, "구글 메일 전송에 실패했습니다."),
 
     /**
      * 403 Forbidden
