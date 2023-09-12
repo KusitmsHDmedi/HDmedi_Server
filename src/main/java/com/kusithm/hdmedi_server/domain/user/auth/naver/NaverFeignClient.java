8442acb397a8c1947a3745be49ff40cdd8f9994b
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-12 15:54:59 +0900

    [fix] #15 네이버 소셜 로그인 요청 url 수정

--- src/main/java/com/kusithm/hdmedi_server/domain/user/auth/naver/NaverFeignClient.java
+++ src/main/java/com/kusithm/hdmedi_server/domain/user/auth/naver/NaverFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "naver-feign-client", url = "https://openapi.naver.com")
+@FeignClient(name = "naver-feign-client", url = "https://openapi.naver.com/v1/nid/me")
 public interface NaverFeignClient {
     @GetMapping
     NaverAccessTokenInfo getNaverAccessTokenInfo(@RequestHeader("Authorization") String accessToken);
