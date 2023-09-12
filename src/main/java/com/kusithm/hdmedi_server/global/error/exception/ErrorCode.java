e73537ea9f6753de259f7e81baff80e09a242df7
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-12 16:56:52 +0900

    [feat] #15 user not found 예외 추가

--- src/main/java/com/kusithm/hdmedi_server/global/error/exception/ErrorCode.java
+++ src/main/java/com/kusithm/hdmedi_server/global/error/exception/ErrorCode.java
@@ -37,6 +37,7 @@ public enum ErrorCode {
      * 404 Not Found
      */
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
 
     /**
      * 405 Method Not Allowed
