b5ef261d46b02a872d27c57d0041e1f774f7afa6
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-13 15:44:13 +0900

    [feat] #14 인증코드 api controller 구현

--- src/main/java/com/kusithm/hdmedi_server/domain/user/controller/AuthController.java
+++ src/main/java/com/kusithm/hdmedi_server/domain/user/controller/AuthController.java
@@ -2,18 +2,18 @@ package com.kusithm.hdmedi_server.domain.user.controller;
 
 import com.kusithm.hdmedi_server.domain.user.dto.request.UserAuthRequestDto;
 import com.kusithm.hdmedi_server.domain.user.dto.request.UserSignUpRequestDto;
+import com.kusithm.hdmedi_server.domain.user.dto.response.AuthCodeResponseDto;
 import com.kusithm.hdmedi_server.domain.user.dto.response.UserAuthResponseDto;
 import com.kusithm.hdmedi_server.domain.user.service.AuthService;
 import com.kusithm.hdmedi_server.global.common.BaseResponse;
 import com.kusithm.hdmedi_server.global.common.SuccessCode;
+import com.kusithm.hdmedi_server.global.config.auth.AuthenticatedUserId;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.criteria.JpaCriteriaUpdate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
@@ -23,7 +23,7 @@ public class AuthController {
 
     @PostMapping("/signIn")
     public ResponseEntity<BaseResponse<?>> signIn(@RequestHeader("Authorization") final String token,
-                                                  @RequestBody final UserAuthRequestDto userAuthRequestDto){
+                                                  @RequestBody final UserAuthRequestDto userAuthRequestDto) {
         final UserAuthResponseDto responseDto = authService.signIn(token, userAuthRequestDto);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(BaseResponse.of(SuccessCode.OK, responseDto));
@@ -31,9 +31,16 @@ public class AuthController {
 
     @PostMapping("/signUp")
     public ResponseEntity<BaseResponse<?>> signUp(@RequestHeader("Authorization") final String token,
-                                                  @RequestBody final UserSignUpRequestDto userSignUpRequestDto){
+                                                  @RequestBody final UserSignUpRequestDto userSignUpRequestDto) {
         final UserAuthResponseDto responseDto = authService.signUp(token, userSignUpRequestDto);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(BaseResponse.of(SuccessCode.OK, responseDto));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BaseResponse.of(SuccessCode.CREATED, responseDto));
+    }
+
+    @GetMapping("/authCode")
+    public ResponseEntity<BaseResponse<?>> createAuthCode(@AuthenticatedUserId final Long userId) {
+        final AuthCodeResponseDto responseDto = authService.createAuthCode(userId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BaseResponse.of(SuccessCode.CREATED, responseDto));
     }
 }
