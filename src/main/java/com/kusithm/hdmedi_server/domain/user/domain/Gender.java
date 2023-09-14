cc22d4534dc1664f5b263cf9747bff86ad576ecb
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-14 15:39:11 +0900

    [feat] #20 children gender enum class 구현

--- src/main/java/com/kusithm/hdmedi_server/domain/user/domain/Gender.java
+++ src/main/java/com/kusithm/hdmedi_server/domain/user/domain/Gender.java
@@ -1,6 +1,25 @@
 package com.kusithm.hdmedi_server.domain.user.domain;
 
+import com.kusithm.hdmedi_server.global.error.exception.InvalidValueException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+import static com.kusithm.hdmedi_server.global.error.exception.ErrorCode.INVALID_GENDER_TYPE;
+
+@RequiredArgsConstructor
+@Getter
 public enum Gender {
-    NAN,
-    WOMAN;
+    MAN("man"),
+    WOMAN("woman");
+
+    private final String stringGender;
+
+    public static Gender getEnumGenderFrom(String stringGender) {
+        return Arrays.stream(values())
+                .filter(gender -> gender.stringGender.equals(stringGender))
+                .findFirst()
+                .orElseThrow(() -> new InvalidValueException(INVALID_GENDER_TYPE));
+    }
 }
