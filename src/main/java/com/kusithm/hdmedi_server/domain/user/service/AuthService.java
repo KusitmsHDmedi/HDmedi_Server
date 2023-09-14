931245905bfa7fd082bc06e6e4565f388bd57a0f
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-14 15:40:37 +0900

    [feat] #20 geust login 서비스 구현

--- src/main/java/com/kusithm/hdmedi_server/domain/user/service/AuthService.java
+++ src/main/java/com/kusithm/hdmedi_server/domain/user/service/AuthService.java
@@ -1,29 +1,32 @@
 package com.kusithm.hdmedi_server.domain.user.service;
 
 import com.kusithm.hdmedi_server.domain.user.auth.naver.NaverOAuthProvider;
-import com.kusithm.hdmedi_server.domain.user.domain.AuthCode;
-import com.kusithm.hdmedi_server.domain.user.domain.Platform;
-import com.kusithm.hdmedi_server.domain.user.domain.User;
+import com.kusithm.hdmedi_server.domain.user.domain.*;
 import com.kusithm.hdmedi_server.domain.user.dto.request.UserAuthRequestDto;
 import com.kusithm.hdmedi_server.domain.user.dto.request.UserSignUpRequestDto;
 import com.kusithm.hdmedi_server.domain.user.dto.response.AuthCodeResponseDto;
+import com.kusithm.hdmedi_server.domain.user.dto.response.GuestSignInResponseDto;
 import com.kusithm.hdmedi_server.domain.user.dto.response.UserAuthResponseDto;
 import com.kusithm.hdmedi_server.domain.user.repository.AuthCodeRepository;
+import com.kusithm.hdmedi_server.domain.user.repository.ChildrenRepository;
 import com.kusithm.hdmedi_server.domain.user.repository.RefreshTokenRepository;
 import com.kusithm.hdmedi_server.domain.user.repository.UserRepository;
+import com.kusithm.hdmedi_server.global.common.HDmediUser;
 import com.kusithm.hdmedi_server.global.config.jwt.JwtProvider;
 import com.kusithm.hdmedi_server.global.config.jwt.Token;
 import com.kusithm.hdmedi_server.global.error.exception.ConflictException;
 import com.kusithm.hdmedi_server.global.error.exception.EntityNotFoundException;
+import com.kusithm.hdmedi_server.global.error.exception.UnauthorizedException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static com.kusithm.hdmedi_server.domain.user.domain.RefreshToken.createRefreshToken;
-import static com.kusithm.hdmedi_server.global.error.exception.ErrorCode.DUPLICATE_USER;
-import static com.kusithm.hdmedi_server.global.error.exception.ErrorCode.USER_NOT_FOUND;
+import static com.kusithm.hdmedi_server.global.error.exception.ErrorCode.*;
 
 @RequiredArgsConstructor
 @Transactional
@@ -32,19 +35,19 @@ public class AuthService {
     private final NaverOAuthProvider naverOAuthProvider;
     private final RefreshTokenRepository refreshTokenRepository;
     private final AuthCodeRepository authCodeRepository;
+    private final ChildrenRepository childrenRepository;
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
-
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final int CODE_LENGTH = 6;
 
     public UserAuthResponseDto signIn(String token, UserAuthRequestDto requestDto) {
         Platform platform = Platform.getEnumPlatformFrom(requestDto.getPlatform());
         String platformId = getPlatformId(token);
-        User findUser = getUser(platform, platformId);
-        Token issuedToken = issueAccessTokenAndRefreshToken(findUser);
+        User findUser = getUserFromPlatformAndPlatformId(platform, platformId);
+        Token issuedToken = issueAccessTokenAndRefreshToken(findUser, Boolean.FALSE);
         updateRefreshToken(issuedToken.getRefreshToken(), findUser);
-        return UserAuthResponseDto.of(issuedToken, findUser);
+        return UserAuthResponseDto.of(issuedToken, findUser, findUser.getChildren());
     }
 
     public UserAuthResponseDto signUp(String token, UserSignUpRequestDto requestDto) {
@@ -52,16 +55,34 @@ public class AuthService {
         String platformId = getPlatformId(token);
         validateDuplicateUser(platform, platformId);
         User saveUser = saveUser(platform, platformId, requestDto.getUserName());
-        Token issuedToken = issueAccessTokenAndRefreshToken(saveUser);
+        Children saveChildren = saveChildren(saveUser, requestDto.getChildrenName(), requestDto.getGender(), requestDto.getBirthday());
+        Token issuedToken = issueAccessTokenAndRefreshToken(saveUser, Boolean.FALSE);
         updateRefreshToken(issuedToken.getRefreshToken(), saveUser);
-        return UserAuthResponseDto.of(issuedToken, saveUser);
+        return UserAuthResponseDto.of(issuedToken, saveUser, saveChildren);
     }
 
-    public AuthCodeResponseDto createAuthCode(Long userId) {
+    public AuthCodeResponseDto createAuthCode(HDmediUser hDmediUser) {
         String authCode = createAuthCodeAtSecureRandom(new SecureRandom());
-        AuthCode createdAuthCode = AuthCode.createAuthCode(authCode, userId);
+        AuthCode createdAuthCode = AuthCode.createAuthCode(authCode, hDmediUser.getId());
         saveAuthCode(createdAuthCode);
         return AuthCodeResponseDto.of(createdAuthCode.getAuthCode());
+    }
+
+    public GuestSignInResponseDto geustSignIn(String authCode){
+        AuthCode findAuthCode = getUserFromAuthCode(authCode);
+        User findUser = getUserFromId(findAuthCode.getId());
+        Token issuedToken = issueAccessTokenAndRefreshToken(findUser, Boolean.TRUE);
+        return GuestSignInResponseDto.of(findUser, findUser.getChildren(), issuedToken.getAccessToken());
+    }
+
+    private User getUserFromId(Long userId){
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
+    }
+
+    private AuthCode getUserFromAuthCode(String authCode){
+        return authCodeRepository.findByAuthCode(authCode)
+                .orElseThrow(() -> new UnauthorizedException(INVALID_AUTH_CODE));
     }
 
     private void saveAuthCode(AuthCode createdAuthCode) {
@@ -90,7 +111,14 @@ public class AuthService {
         return codeBuilder;
     }
 
-    private User getUser(Platform platform, String platformId) {
+    private Children saveChildren(User user, String name, String stringGender, LocalDate birthday){
+        Gender enumGender = Gender.getEnumGenderFrom(stringGender);
+        Children createdChildren = Children.createChildren(user, name, enumGender, birthday);
+        childrenRepository.save(createdChildren);
+        return createdChildren;
+    }
+
+    private User getUserFromPlatformAndPlatformId(Platform platform, String platformId) {
         return userRepository.findUserByPlatformAndPlatformId(platform, platformId)
                 .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
     }
@@ -105,8 +133,9 @@ public class AuthService {
         return naverOAuthProvider.getNaverPlatformId(token);
     }
 
-    private Token issueAccessTokenAndRefreshToken(User user) {
-        return jwtProvider.issueToken(user.getId());
+    private Token issueAccessTokenAndRefreshToken(User user, Boolean isGuest) {
+        HDmediUser hDmediUser = HDmediUser.of(user.getId(), isGuest);
+        return jwtProvider.issueToken(hDmediUser);
     }
 
     private void updateRefreshToken(String refreshToken, User user) {
