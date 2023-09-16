280e72f5cd62e0a0f11c103993d6347d577f2bfc
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-17 05:06:41 +0900

    [fix] #40 네이버 access token 회원가입 로직 수정

--- src/main/java/com/kusithm/hdmedi_server/domain/user/auth/naver/NaverOAuthProvider.java
+++ src/main/java/com/kusithm/hdmedi_server/domain/user/auth/naver/NaverOAuthProvider.java
@@ -12,22 +12,11 @@ import static com.kusithm.hdmedi_server.global.error.exception.ErrorCode.INVALID
 @RequiredArgsConstructor
 @Component
 public class NaverOAuthProvider {
-    private final NaverFeignClient naverFeignClient;
-
     public String getNaverPlatformId(String accessToken) {
         NaverAccessToken naverAccessToken = NaverAccessToken.of(accessToken);
         String accessTokenWithTokenType = naverAccessToken.getAccessTokenWithTokenType();
-        NaverAccessTokenInfo naverAccessTokenInfo = getNaverAccessTokenInfo(accessTokenWithTokenType);
-        return String.valueOf(naverAccessTokenInfo.getId());
-    }
-
-    private NaverAccessTokenInfo getNaverAccessTokenInfo(String accessTokenWithTokenType) {
-        try {
-            return naverFeignClient.getNaverAccessTokenInfo(accessTokenWithTokenType);
-        } catch (FeignException e) {
-            log.error("Feign Exception: ", e);
-            throw new UnauthorizedException(INVALID_NAVER_ACCESS_TOKEN);
-        }
+        NaverAccessTokenInfo naverAccessTokenInfo = NaverAccessTokenInfo.of(accessTokenWithTokenType);
+        return naverAccessTokenInfo.getAccessToken();
     }
 
 }
