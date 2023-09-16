ffd1faf6359e110bd4af427a9b59c02359c076f6
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-16 21:47:16 +0900

    [chore] #28 mongo 애노테이션 추가

--- src/main/java/com/kusithm/hdmedi_server/HDmediServerApplication.java
+++ src/main/java/com/kusithm/hdmedi_server/HDmediServerApplication.java
@@ -3,8 +3,14 @@ package com.kusithm.hdmedi_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class HDmediServerApplication {
     // aws sdk 연동
     static {
